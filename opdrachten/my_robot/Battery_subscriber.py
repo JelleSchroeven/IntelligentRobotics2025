43b3858e47c43e3da749a920e6e5cedb79685798
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-06 12:14:51 +0100

    Create Battery_subscriber.py
    battery subscriber
    waneer onder 11,5v  geeft asap charge melding
    anders gewoon voltage

---
+++ opdrachten/my_robot/Battery_subscriber.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
+class BatterySubscriber(Node):
+    def __init__(self):
+        super().__init__('battery_subscriber')
+        self.subscription = self.create_subscription(
+            Float32,
+            'battery_percentage',
+            self.battery_callback,
+            10)
+        self.subscription 
+
+    def battery_callback(self, msg):
+        voltage = msg.data
+        self.get_logger().info(f'Battery voltage: {voltage:.2f}V')
+        if voltage < 11.5:
+            self.get_logger().warn('Battery voltage low recharge ASAP.')
+    
+    def main(args=None):
+        rclpy.init(args=args)
+        battery_subscriber = BatterySubscriber()
+        rclpy.spin(battery_subscriber)
+        battery_subscriber.destroy_node()
+        rclpy.shutdown()
+    
+    if __name__ == '__main__':
+        main()
