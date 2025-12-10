72dca8af26f7703528a201dae1a78e2c98261d61
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-10 19:21:51 +0100

    Update keyboard_publisher.py
    get key en main  en loopfunction

--- opdrachten/my_robot/keyboard_publisher.py
+++ opdrachten/my_robot/keyboard_publisher.py
@@ -20,4 +20,49 @@ class KeyboardPublisher:
         self._thread = threading.Thread(target=self._loop, daemon=True)
         self._thread.start()
 
-    
+    def _get_key(self, timeout=0.1):
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select.select([sys.stdin], [], [], timeout)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        return key
+    def _loop(self):
+        try:
+            while rclpy.ok() and self.running:
+                key=self._get_key()
+                if not key:
+                    time.sleep(0.1)
+                    continue
+                if key == '\x03':  # Ctrl-C
+                    break
+                msg = String()
+                msg.data = key
+                self.pub.publish(msg)
+                self.get_logger().info(f'Key pressed: {key}')
+        finally:
+            stop_msg = String()
+            stop_msg.data = 'programma gestopt'
+            try:
+                self.pub.publish(stop_msg)
+            except Exception as e:
+                pass
+
+def main(args=None):
+    rclpy.init(args=args)
+    keyboard_publisher = KeyboardPublisher()
+    try:
+        rclpy.spin(keyboard_publisher)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        try:
+            Node.destroy_node()
+        except Exception as e:
+            pass
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
